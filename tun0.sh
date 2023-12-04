46d88c0057dc096cabe06204f06468680ac1aa4f
Guofang Wang <uglycat@foxmail.com>
2023-12-04 11:14:38 +0800

    Delete tun0.sh

--- tun0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TUN=tun0
-sudo ip tuntap add mode tun dev $TUN user $USER
-sudo ip link set $TUN up
-sudo ip addr add 192.168.5.1/24 dev $TUN
-#sudo ip link set mtu 1452 $TUN
-#sudo ip addr add 192.168.5.1/24 peer 192.168.5.2 dev $TUN
